a7c70a8ea4c9f8914b5019d41f20fd5ea5b48760
deveshdasandroid <deveshdas3dv@gmail.com>
2021-02-26 11:08:55 +0530

    fixed permission issue

--- app/src/main/java/com/dasher/dashermusicplayer/MainActivity.java
+++ app/src/main/java/com/dasher/dashermusicplayer/MainActivity.java
@@ -38,16 +38,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	private float slideOffset;
 	private static Toolbar toolBar;
 
-	public boolean checkPermission(){
-		if(ContextCompat.checkSelfPermission(mContext,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-			ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_REQUEST_CODE);
-			Toast.makeText(mContext,"Please grant storage permission",2000).show();
-			return false;
-		}else{
-			return true;
-		}
-	}
-
 	public static void hideOrShowActionBar(boolean show){
 		if(show){
 			tabLayout.setVisibility(View.VISIBLE);
@@ -65,48 +55,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.main);
 	
 		this.mContext = getApplicationContext();
-		MusicManager.setContext(mContext);
 		
 		toolBar = (Toolbar) findViewById(R.id.appbarlayout_tool_bar);
-		setSupportActionBar(toolBar);
-		getSupportActionBar().show();
-		
+		tabLayout = (TabLayout) findViewById(R.id.tab_layout);
 		play = (ImageView) findViewById(R.id.mainImageView1);
-		play.setOnClickListener(this);
-	
 		albumArtSmall = (ImageView) findViewById(R.id.botombarxmlImageView1);
 		currentPlayingSongName = (TextView) findViewById(R.id.botombar_xmlTextView);
+		layout= (SlidingUpPanelLayout)findViewById(R.id.sliding_layout);
+		viewPager = (ViewPager) findViewById(R.id.mainViewPager1);
 		
-		layout= (SlidingUpPanelLayout)
-			findViewById(R.id.sliding_layout);
-		
-		
-		tabLayout = (TabLayout) findViewById(R.id.tab_layout);
 		tabLayout.addTab(tabLayout.newTab().setText("Tracks"));
 		tabLayout.addTab(tabLayout.newTab().setText("Artists"));
+	
+		if(ContextCompat.checkSelfPermission(mContext,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+			ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_REQUEST_CODE);
+		}else{
+			init();
+		}
 
-		viewPager = (ViewPager) findViewById(R.id.mainViewPager1);
-		CustomPagerAdapter cpafvp = new CustomPagerAdapter(getSupportFragmentManager(),2);
+		setSupportActionBar(toolBar);
+		getSupportActionBar().show();
+	
+		play.setOnClickListener(this);
+		
+	}
+
+	private void init()
+	{
+		CustomPagerAdapter cpafvp = new CustomPagerAdapter(getSupportFragmentManager(), 2);
 		viewPager.setAdapter(cpafvp);
 		viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout)); 
 
-		tabLayout.setOnTabSelectedListener( new TabLayout.OnTabSelectedListener( ){
-				@Override
-				public void onTabSelected( TabLayout.Tab p1 )
-				{
-					viewPager.setCurrentItem(p1.getPosition()); 
-				}
-				@Override
-				public void onTabUnselected( TabLayout.Tab p1 )
-				{
-					// TODO: Implement this method
-				}
-				@Override
-				public void onTabReselected( TabLayout.Tab p1 )
-				{
-					// TODO: Implement this method
-				}
-			});
+		tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+			@Override
+			public void onTabSelected(TabLayout.Tab p1) { viewPager.setCurrentItem(p1.getPosition()); }
+			@Override
+			public void onTabUnselected(TabLayout.Tab p1) {}
+			@Override
+			public void onTabReselected(TabLayout.Tab p1) {}
+		});
 		
 		layout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener(){
 			@Override
@@ -114,50 +101,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 			{
 				slideOffset = p2;
 				// TODO: Implement this method
-				if(layout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED){
-					play.setVisibility(View.GONE);
-					play.setClickable(false);
-					isExpanded = true;
-				}else if(layout.getPanelState() == SlidingUpPanelLayout.PanelState.COLLAPSED){
-					play.setVisibility(View.VISIBLE);
-					play.setClickable(true);
-					isExpanded = false;
-				}else if(p2 > 0.0f && p2 < 1.0f){
-					if(isExpanded){
-						play.setAlpha(0.f + p2);
-					}else{
-						play.setAlpha(1.f - p2);
-					}
+				if (layout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED) { play.setVisibility(View.GONE); isExpanded = true; }
+				else if (layout.getPanelState() == SlidingUpPanelLayout.PanelState.COLLAPSED) { play.setVisibility(View.VISIBLE); isExpanded = false; }
+				else if (p2 > 0.0f && p2 < 1.0f) {
+					if (isExpanded) { play.setAlpha(0.f + p2); }
+					else { play.setAlpha(1.f - p2); }
 				}
 			}
-
+			
 			@Override
 			public void onPanelStateChanged(View p1, SlidingUpPanelLayout.PanelState p2, SlidingUpPanelLayout.PanelState p3)
 			{
 				// TODO: Implement this method
-				if(slideOffset<0.2&& p2 == SlidingUpPanelLayout.PanelState.DRAGGING)
-				{
-					layout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
-				}
-				else if(slideOffset == 1&& p2 == SlidingUpPanelLayout.PanelState.DRAGGING)
-				{
-					layout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-				}
+				if (slideOffset < 0.2 && p2 == SlidingUpPanelLayout.PanelState.DRAGGING) { layout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED); }
+				else if (slideOffset == 1 && p2 == SlidingUpPanelLayout.PanelState.DRAGGING) { layout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED); }
 			}
 		});
-	
-		if(!MusicService.isPlaying()){
-			MusicManager.createSong();
-		}
-	
-		if(MusicService.isPlaying()){
-			this.setPlayPauseView(true);
-		}else{
-			this.setPlayPauseView(false);
-		}
-	
+
+		if (!MusicService.isPlaying()) { MusicManager.createSong(); }
+		if (MusicService.isPlaying()) { this.setPlayPauseView(true); }
+		else { this.setPlayPauseView(false); }
 		this.setCurrentPlayingSongData();
-		
 	}
 
 	@Override
@@ -197,9 +161,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 		if(requestCode == STORAGE_REQUEST_CODE && grantResults.length > 0){
 			if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-				Toast.makeText(getApplicationContext(),"Permission Granted",2000).show();
+				Toast.makeText(getApplicationContext(),"Permission Granted, Thank you!",2000).show();
+				init();
 			}else{
-				Toast.makeText(getApplicationContext(),"Permission Denied",2000).show();
+				Toast.makeText(getApplicationContext(),"Permission Denied, We need storage permission to access songs from storage",2000).show();
 				ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_REQUEST_CODE);
 			}
 		}
